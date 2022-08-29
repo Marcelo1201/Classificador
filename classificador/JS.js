72c00e4ff866955f02e036d8517532e44b1688e9
Marcelo Giacometti <marcelo.giacomettigemeo1@gmail.com>
2022-08-28 23:56:11 -0300

    acrescentando Js

--- classificador/JS.js
+++ classificador/JS.js
@@ -1,4 +1,23 @@
+function botaoclick(){
+    var botaoclicado = document.getElementById('avaliar')
+    botaoclicado.style.background = '#942807'
+}
+
+function entrar(){
+    var entrou = document.getElementById('idenviar')
+    entrou.style.background = 'var(--cor-principal)'
+    entrou.style.color = 'var(--cor-vermelho-site)'
+}
+
+function sair(){
+    var saiu = document.getElementById('idenviar')
+    saiu.style.background = 'var(--cor-vermelho-site)'
+    saiu.style.color = 'var(--cor-principal)'
+}
+
 function clicar(){
-    var click = document.getElementById('avaliar')
-    click.style.background = 'red'
+    var clicou = document.getElementById('conteudo-avalie')
+    var clicouum = document.getElementById('conteudo')
+    clicou.style.display = 'block'
+    clicouum.style.display = 'none'
 }
